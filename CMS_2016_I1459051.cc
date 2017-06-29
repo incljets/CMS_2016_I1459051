ce0c47a3d5caba10f337c9c4f8cd6a9ef377c58d
Andy Buckley <andy.buckley@cern.ch>
2017-06-29 14:59:21 +0100

    Adapted to many jet radii and a cleverer histogram storage

--- CMS_2016_I1459051.cc
+++ CMS_2016_I1459051.cc
@@ -2,83 +2,90 @@
 #include "Rivet/Analysis.hh"
 #include "Rivet/Projections/FinalState.hh"
 #include "Rivet/Projections/FastJets.hh"
-#include "Rivet/Tools/BinnedHistogram.hh"
+#include <tuple>
 
 namespace Rivet {
+
 
   // Inclusive jet pT
   class CMS_2016_I1459051 : public Analysis {
   public:
 
     // Constructor
-    CMS_2016_I1459051() : Analysis("CMS_2016_I1459051") {}
+    DEFAULT_RIVET_ANALYSIS_CTOR(CMS_2016_I1459051);
+
+    // Jet radii
+    static constexpr array<double,6> JET_RADII = {0.2, 0.3, 0.4, 0.5, 0.6, 0.7};
+    // |y| bin edges
+    static constexpr array<double,8> RAP_BINEDGES = {0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 4.7};
 
 
     // Book histograms and initialize projections:
     void init() {
       const FinalState fs;
 
-      // Initialize the projectors:
-      addProjection(FastJets(fs, FastJets::ANTIKT, 0.4),"JetsAK4");
-      addProjection(FastJets(fs, FastJets::ANTIKT, 0.7),"JetsAK7");
+      for (double R : JET_RADII) {
 
-      // Book histograms:
-      _hist_sigmaAK4.addHistogram(0.0, 0.5, bookHisto1D(8, 1, 1));
-      _hist_sigmaAK4.addHistogram(0.5, 1.0, bookHisto1D(9, 1, 1));
-      _hist_sigmaAK4.addHistogram(1.0, 1.5, bookHisto1D(10, 1, 1));
-      _hist_sigmaAK4.addHistogram(1.5, 2.0, bookHisto1D(11, 1, 1));
-      _hist_sigmaAK4.addHistogram(2.0, 2.5, bookHisto1D(12, 1, 1));
-      _hist_sigmaAK4.addHistogram(2.5, 3.0, bookHisto1D(13, 1, 1));
-      
-      _hist_sigmaAK7.addHistogram(0.0, 0.5, bookHisto1D(1, 1, 1));
-      _hist_sigmaAK7.addHistogram(0.5, 1.0, bookHisto1D(2, 1, 1));
-      _hist_sigmaAK7.addHistogram(1.0, 1.5, bookHisto1D(3, 1, 1));
-      _hist_sigmaAK7.addHistogram(1.5, 2.0, bookHisto1D(4, 1, 1));
-      _hist_sigmaAK7.addHistogram(2.0, 2.5, bookHisto1D(5, 1, 1));
-      _hist_sigmaAK7.addHistogram(2.5, 3.0, bookHisto1D(6, 1, 1));
-      
-      _hist_sigmaAK7Forward = bookHisto1D(7, 1, 1);
-      _hist_sigmaAK4Forward = bookHisto1D(14, 1, 1);
-      
+        // Initialize the projectors:
+        const size_t iR = size_t(10 * R);
+        declare(FastJets(fs, FastJets::ANTIKT, R), "JetsAK" + toString(iR));
+
+        // Book histograms:
+        for (size_t iy = 0; iy < RAP_BINEDGES.size()-1; ++iy) {
+          _hists[make_tuple(iR, iy)] =
+            bookHisto1D("pT_R" + toString(iR) + "_y"
+                        + toString(RAP_BINEDGES[iy]) + "_" + toString(RAP_BINEDGES[iy]),
+                        refData(iy+1, 1, 1));
+        }
+      }
+
     }
+
 
     // Analysis
-    void analyze(const Event &event) {
+    void analyze(const Event& event) {
       const double weight = event.weight();
-      const FastJets& fjAK4 = applyProjection<FastJets>(event,"JetsAK4");
-      const FastJets& fjAK7 = applyProjection<FastJets>(event,"JetsAK7");
-      const Jets& jetsAK4 = fjAK4.jets(Cuts::ptIn(114*GeV, 2200.0*GeV) && Cuts::absrap < 4.7);
-      const Jets& jetsAK7 = fjAK7.jets(Cuts::ptIn(114*GeV, 2200.0*GeV) && Cuts::absrap < 4.7);
 
-      // Fill the relevant histograms:
-      foreach(const Jet& j, jetsAK4) {
-        _hist_sigmaAK4.fill(j.absrap(), j.pT(), weight);
-	if(j.absrap()>3.2 && j.absrap()<4.7) _hist_sigmaAK4Forward->fill(j.pT(), weight);
-      }
+      for (double R : JET_RADII) {
+        const size_t iR = size_t(10 * R);
 
-      foreach(const Jet& j, jetsAK7) {
-        _hist_sigmaAK7.fill(j.absrap(), j.pT(), weight);
-	if(j.absrap()>3.2 && j.absrap()<4.7) _hist_sigmaAK7Forward->fill(j.pT(), weight);
+        // Get jets
+        const FastJets& fj = apply<FastJets>(event, "JetsAK" + toString(iR));
+        const Jets& jets = fj.jets(Cuts::ptIn(114*GeV, 2200.0*GeV) && Cuts::absrap < 4.7);
+
+        // Fill histograms
+        for (const Jet& j : jets) {
+
+          // Safe index decoding from Rivet 2.6.0:
+          // const int iy = binIndex(j.absrap(), RAP_BINEDGES);
+          // if (iy < 0) continue;
+          // For now:
+          if (!inRange(j.absrap(), RAP_BINEDGES.front(), RAP_BINEDGES.back())) continue; // outside range
+          const size_t iy = std::distance(RAP_BINEDGES.begin(), std::upper_bound(RAP_BINEDGES.begin(), RAP_BINEDGES.end(), j.absrap()));
+
+          _hists[make_tuple(iR, iy)]->fill(j.pT()/GeV, weight);
+        }
       }
 
     }
+
 
     // Finalize
     void finalize() {
-      _hist_sigmaAK4.scale(crossSection()/sumOfWeights()/2.0, this);
-      _hist_sigmaAK7.scale(crossSection()/sumOfWeights()/2.0, this);
-      scale(_hist_sigmaAK4Forward,crossSection()/sumOfWeights()/3.0);
-      scale(_hist_sigmaAK7Forward,crossSection()/sumOfWeights()/3.0);
+      for (auto k_hptr : _hists)
+        scale(k_hptr.second, crossSection()/sumOfWeights()/2.0);
     }
 
+
   private:
-    BinnedHistogram<double> _hist_sigmaAK4;
-    BinnedHistogram<double> _hist_sigmaAK7;
-    Histo1DPtr _hist_sigmaAK4Forward;
-    Histo1DPtr _hist_sigmaAK7Forward;
+
+    map<tuple<size_t,size_t>, Histo1DPtr> _hists;
+
   };
 
-  // This global object acts as a hook for the plugin system.
+
+  // Hook for the plugin system.
   DECLARE_RIVET_PLUGIN(CMS_2016_I1459051);
+
 
 }
