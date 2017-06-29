7b9d8c9531acda37526118afc67fb53981eca8b8
Andy Buckley <andy.buckley@cern.ch>
2017-06-29 15:20:53 +0100

    Add angularities

--- CMS_2016_I1459051.cc
+++ CMS_2016_I1459051.cc
@@ -2,7 +2,7 @@
 #include "Rivet/Analysis.hh"
 #include "Rivet/Projections/FinalState.hh"
 #include "Rivet/Projections/FastJets.hh"
-#include <tuple>
+#include <tuple> //< remove from R2.6
 
 namespace Rivet {
 
@@ -12,7 +12,7 @@ namespace Rivet {
   public:
 
     // Jet radii
-    static constexpr array<double,6> JET_RADII = {0.2, 0.3, 0.4, 0.5, 0.6, 0.7};
+    static constexpr array<double,6> JET_RADII = {0.3, 0.4, 0.5, 0.6, 0.7, 1.0};
     // |y| bin edges
     static constexpr array<double,8> RAP_BINEDGES = {0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 4.7};
 
@@ -56,14 +56,31 @@ namespace Rivet {
         // Fill histograms
         for (const Jet& j : jets) {
 
+          // pT spectra in |y| bins
           // Safe index decoding from Rivet 2.6.0:
           // const int iy = binIndex(j.absrap(), RAP_BINEDGES);
           // if (iy < 0) continue;
           // For now:
           if (!inRange(j.absrap(), RAP_BINEDGES.front(), RAP_BINEDGES.back())) continue; // outside range
           const size_t iy = std::distance(RAP_BINEDGES.begin(), std::upper_bound(RAP_BINEDGES.begin(), RAP_BINEDGES.end(), j.absrap()));
-
           _hists[make_tuple(iR, iy)]->fill(j.pT()/GeV, weight);
+
+          // Angularities
+          double scalar_pt = 0, sum1020 = 0, sum1010 = 0, sum1005 = 0, sum0000 = 0, sum2000 = 0;
+          for (const Particle& p : jets.front().particles()) {
+            scalar_pt += p.pT();
+            sum1020 += pow(p.pT(), 2.0) * pow(deltaR(p, jets[0]), 1.0);
+            sum1010 += pow(p.pT(), 1.0) * pow(deltaR(p, jets[0]), 1.0);
+            sum1005 += pow(p.pT(), 0.5) * pow(deltaR(p, jets[0]), 1.0);
+            sum0000 += pow(p.pT(), 0.0) * pow(deltaR(p, jets[0]), 0.0);
+            sum2000 += pow(p.pT(), 0.0) * pow(deltaR(p, jets[0]), 2.0);
+          }
+          const double ga1020 = sum1020 / pow(scalar_pt, 2.0) * pow(R, 1.0);
+          const double ga1010 = sum1010 / pow(scalar_pt, 1.0) * pow(R, 1.0);
+          const double ga1005 = sum1005 / pow(scalar_pt, 0.5) * pow(R, 1.0);
+          const double ga0000 = sum0000 / pow(scalar_pt, 0.0) * pow(R, 1.0);
+          const double ga2000 = sum2000 / pow(scalar_pt, 0.0) * pow(R, 2.0);
+
         }
       }
 
